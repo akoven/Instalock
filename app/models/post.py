923cdbf41f99fbd639783881ba28bc6e62f5beda
Thomas Anton <thomasanton1224@gmail.com>
2022-08-19 19:07:10 -0400

    posts models done

---
+++ app/models/post.py
@@ -0,0 +1,30 @@
+from .db import db
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    caption = db.Column(db.String(2200), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    likes = db.Column(db.Integer, default=0)
+    image_url = db.Column(db.String, nullable=False)
+
+    user = db.relationship("User", back_populates="posts")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "caption": self.caption,
+            "user_id": self.user_id,
+            "likes": self.likes,
+            "image_url": self.image_url,
+        }
+
+    def __repr__(self):
+        return f"""
+            < Post ID: {self.id}\n
+              Caption: {self.caption}\n
+              User ID: {self.user_id}\n
+              Likes: {self.likes}\n
+              Image URL: {self.image_url} >
+            """
