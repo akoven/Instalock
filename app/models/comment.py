36a9572a4b53cfb9f25ec515b9903c22ac4729cc
Thomas Anton <thomasanton1224@gmail.com>
2022-08-22 13:30:59 -0400

    post seed data complete

--- app/models/comment.py
+++ app/models/comment.py
@@ -7,6 +7,7 @@ class Comment(db.Model):
     content = db.Column(db.String(2200), nullable=False)
     user_id = db.Column(db.Integer, foreign_key=('users.id') nullable=False)
     post_id = db.Column(db.Integer, foreign_key=('posts.id') nullable=False)
+    likes = db.Column(db.Integer, default=0)
 
     post = db.relationship("Post", back_populates="comments")
     user = db.relationship("User", back_populates="comments")
