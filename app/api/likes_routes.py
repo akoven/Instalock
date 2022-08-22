f7c7ab947ae32ca912bc44adbe252e34c82b4407
Attiya <adarensburg@live.com>
2022-08-22 18:48:01 -0500

    like_routes completed

--- app/api/likes_routes.py
+++ app/api/likes_routes.py
@@ -1,19 +1,116 @@
 from flask import Blueprint
 from app.models import db
-from app.models.likes import Likes
+from app.models import Comment, Post
+from app.models.likes import Like
 from app.models.post import Post
+from app.forms import LikeForm, PostForm
 from flask_login import current_user
 likes_routes = Blueprint("likes",__name__,url_prefix="/likes")
 
-@likes_routes.route('/user/<int:id>', methods=["GET","DELETE"])
-def likes():
-    if current_user:
-        all_likes = Likes.query.all()
-        return all_likes
-    for single_like in Post.data['likes']:
-        if single_like == 1:
-            db.session.add(single_like)
-        else:
-            db.session.delete(single_like)
+# @likes_routes.route('/<post_id>', methods=["POST"])
+# def post_likes():
+#     if current_user:
+#         all_likes = Like.query.all()
+#         return all_likes
 
-    db.session.commit()
+# @likes_routes.route('/<comment_id>', methods=["POST"])
+# def comment_likes():
+#     if current_user:
+#         all_likes = Like.query.all()
+#         return all_likes
+
+@likes_routes.route('/<comment_id>', method=["POST"])
+def comment_likes(comment_id):
+    form = LikeForm()
+
+    content = form.data['content']
+    user_id = form.data['user_id']
+    post_id = form.data['post_id']
+    comment_id = form.data['comment_id']
+    total_likes = Comment.likes
+
+    if comment_id:
+        total_likes += 1
+        comment = Comment(
+            content = content,
+            user_id = user_id,
+            post_id = post_id,
+            likes = total_likes
+        )
+
+        db.session.add(comment)
+        db.session.commit()
+    else:
+        'something went wrong'
+@likes_routes.route('/<post_id>', method=["POST"])
+def post_likes(post_id):
+    form = Post()
+
+    caption = form.data['caption']
+    user_id = form.data['user_id']
+    likes = form.data['likes']
+    image_url = form.data['image_url']
+    total_likes = Post.likes
+
+    if post_id:
+        total_likes += 1
+
+        post = Post(
+            caption = caption,
+            user_id = user_id,
+            likes = total_likes,
+            image_url = image_url,
+        )
+
+        db.session.add(post)
+        db.session.commit()
+    else:
+        'something went wrong'
+
+@likes_routes.route('<post_id>', methods=["DELETE"])
+def delete_post_like(post_id):
+    form = Post()
+
+    caption = form.data['caption']
+    user_id = form.data['user_id']
+    likes = form.data['likes']
+    image_url = form.data['image_url']
+    total_likes = Post.likes
+
+    if post_id:
+        total_likes -= 1
+
+        post = Post(
+            caption = caption,
+            user_id = user_id,
+            likes = total_likes,
+            image_url = image_url,
+            total_likes = total_likes
+        )
+        db.session.add(post)
+        db.session.commit()
+    else:
+        return 'Something went wrong'
+
+@likes_routes.route('<comment_id>', methods=["DELETE"])
+def delete_comment_like():
+    form = LikeForm()
+
+    content = form.data['content']
+    user_id = form.data['user_id']
+    post_id = form.data['post_id']
+    comment_id = form.data['comment_id']
+    total_likes = Comment.likes
+
+    if comment_id:
+        total_likes -= 1
+        comment = Comment(
+            content = content,
+            user_id = user_id,
+            post_id = post_id,
+            likes = total_likes
+        )
+        db.session.add(comment)
+        db.session.commit()
+    else:
+        return 'Something went wrong'
--- app/models/likes.py
+++ app/models/likes.py
@@ -1,20 +1,17 @@
-# from .db import db
+from .db import db
 
 
-# class Like(db.Model):
-#     __tablename__ = "likes"
+class Like(db.Model):
+    __tablename__ = "likes"
 
-#     id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
 
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-#     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
-#     comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'), nullable=False)
-
-
-#     def to_dict(self):
-#         return {
-#             "likes": self.likes
-#         }
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=True)
+    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'), nullable=True)
 
 
-
+    def to_dict(self):
+        return {
+            "likes": self.likes
+        }
