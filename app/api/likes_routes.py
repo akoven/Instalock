0efb7c07e4ab0c3f18e822beb5cf4d5375b769da
Attiya <adarensburg@live.com>
2022-08-22 12:49:46 -0500

    like_routes still needs the likes model to get rid of warning

--- app/api/likes_routes.py
+++ app/api/likes_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from app.models import db
 from app.models.likes import Likes
+from app.models.post import Post
 from flask_login import current_user
 likes_routes = Blueprint("likes",__name__,url_prefix="/likes")
 
@@ -9,3 +10,10 @@ def likes():
     if current_user:
         all_likes = Likes.query.all()
         return all_likes
+    for single_like in Post.data['likes']:
+        if single_like == 1:
+            db.session.add(single_like)
+        else:
+            db.session.delete(single_like)
+
+    db.session.commit()
