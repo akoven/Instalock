06a6312cdda54f37c8e018a79324c10c5a3106b1
Attiya <adarensburg@live.com>
2022-08-23 18:46:55 -0500

    added optional chaining in all parameters

--- react-app/src/components/profile_pg/ProfilePage.js
+++ react-app/src/components/profile_pg/ProfilePage.js
@@ -29,10 +29,10 @@ const ProfilePage = () => {
 
     return(
        <>
-        <h3>{userProfile?.profile.username}</h3>
+        <h3>{userProfile?.profile?.username}</h3>
         <p>Followers: {userProfile?.follower_count}</p>
         <p>Following: {userProfile?.following_count}</p>
-        <p>{userProfile?.profile.bio}</p>
+        <p>{userProfile?.profile?.bio}</p>
        </>
 
     )
