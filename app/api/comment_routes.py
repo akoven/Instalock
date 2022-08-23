05ad0c8f6ebaacf7cbef678e80caadd92a7182f6
Waseemalame <waseemalame45@gmail.com>
2022-08-23 10:04:02 -0700

    error handling comments PUT and DELETE

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -3,6 +3,7 @@ from app.models import db, User, Post, Comment
 from app.forms import CommentForm
 from flask_login import current_user
 
+
 comment_routes = Blueprint("comments", __name__, url_prefix="/comments")
 
 @comment_routes.route("/<comment_id>", methods=["PUT"])
@@ -11,25 +12,25 @@ def edit_comment(comment_id):
 
     comment_form = CommentForm()
     comment_form['csrf_token'].data = request.cookies['csrf_token']
-    if comment_form.validate_on_submit():
-        comment = Comment.query.get(comment_id)
+    comment = Comment.query.get(comment_id)
+
+    if current_user.id == comment.user_id:
 
         comment.content = comment_form.data["content"]
 
         db.session.commit()
         return comment.to_dict()
     else:
-        return "Didn't make it to the if"
+         return '404: unauthorized user'
 
 @comment_routes.route("/<comment_id>", methods=["DELETE"])
 def delete_item(comment_id):
-    #TODO user auth for deletion
     comment = Comment.query.get(comment_id)
-    if current_user.is_authenticated and current_user.get_id() == comment.user_id:
+    if current_user.id == comment.user_id:
 
         db.session.delete(comment)
         db.session.commit()
 
         return "Successfully Deleted"
     else:
-        return "User not logged in"
+        return '404: unauthorized user'
