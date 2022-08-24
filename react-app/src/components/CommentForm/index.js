2e3234f608216fb99f51c56827b78597107d4f56
Waseemalame <waseemalame45@gmail.com>
2022-08-24 15:39:53 -0700

    comment form component complete

--- react-app/src/components/CommentForm/index.js
+++ react-app/src/components/CommentForm/index.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const CommentForm = ({ post }) => {
 const [commentContent, setCommentContent] = useState('');
 const updateComment = (e) => setCommentContent(e.target.value)
+
+const userSession = useSelector(state => state.session.user)
 
 
 
@@ -11,8 +13,10 @@ const updateComment = (e) => setCommentContent(e.target.value)
     e.preventDefault();
     const data = {
       content: commentContent,
+      post_id: post.id,
+      user_id: userSession.id
     }
-
+    console.log(data, 'data')
   }
   return (
     <form action="" className="comment-form" onSubmit={(e) => commentSubmit(e, post.id)}>
