7fafc9d07b17ddf87f0c2d24a2c15c5c2fa41f37
Thomas Anton <thomasanton1224@gmail.com>
2022-08-24 16:18:27 -0400

    lots of store changes and edit post almost finished

--- react-app/src/components/SinglePostComponents/EditPostModal/index.js
+++ react-app/src/components/SinglePostComponents/EditPostModal/index.js
@@ -1,18 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Modal } from '../../../context/Modal'
+import { getPostsThunk } from '../../../store/post'
 import EditPostForm from './EditPostForm'
+import './EditPost.css'
 
 function EditPostModal() {
+    const dispatch = useDispatch()
     const [showModal, setShowModal] = useState(false)
+    const post = useSelector(state => state.posts[1])
+
+
+    useEffect(() => {
+        dispatch(getPostsThunk())
+    }, [dispatch])
 
     return (
         <>
-        <div className='edit-post-button' onClick={setShowModal(true)}>Edit</div>
-            {showModal && (
-                <Modal onClose={() => setShowModal(false)}>
-                    <EditPostForm />
-                </Modal>
-            )}
+        <div className='edit-post-button' onClick={() => setShowModal(true)}>Edit</div>
+        {showModal && (
+            <Modal onClose={() => setShowModal(false)}>
+                <EditPostForm post={post} />
+            </Modal>
+        )}
         </>
     )
 }
--- react-app/src/store/post.js
+++ react-app/src/store/post.js
@@ -1,9 +1,17 @@
 const GET_ALL_POSTS = "posts/get-user-posts"
+const UPDATE_POST = "posts/update-post"
 
 const getAllPosts = (allPosts) => {
     return {
         type: GET_ALL_POSTS,
         allPosts
+    }
+}
+
+const updatePost = (post) => {
+    return {
+        type: UPDATE_POST,
+        post
     }
 }
 
@@ -26,7 +34,22 @@ export const getPostsThunk = () => async dispatch => {
     const posts = await res.json();
     dispatch(getAllPosts(posts));
     return res;
-  }
+}
+
+export const updatePostThunk = (payload, postId) => async dispatch => {
+    const res = await fetch(`/api/posts/${postId}`, {
+        method: 'PUT',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload)
+    })
+
+    if (res.ok) {
+        const post = await res.json()
+        dispatch(updatePost(post))
+        return post
+    }
+}
+
 
 const initialState = {};
 const postsReducer = (state = initialState, action) => {
@@ -37,6 +60,10 @@ const postsReducer = (state = initialState, action) => {
             action.allPosts.posts.forEach((post) => posts[post.id] = post)
             return posts;
         }
+        case UPDATE_POST:
+            let newState = {...state};
+            newState[action.post.id] = action.post;
+            return newState;
         default:
             return state;
     }
