20fc2183a0dac4f93ddc3f64a27217bcb165a8e2
Waseemalame <waseemalame45@gmail.com>
2022-08-25 15:53:17 -0700

    z-index to nav menu & fixed logout redirect bug

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { logout } from '../../store/session';
 import "../NavBar/NavBar.css"
 
 const LogoutButton = () => {
+  const history = useHistory();
   const dispatch = useDispatch()
   const onLogout = async (e) => {
+    history.push("/login")
     await dispatch(logout());
+
   };
 
   return <button className="logout-button" onClick={onLogout}>Logout</button>;
