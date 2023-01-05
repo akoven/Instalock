e7d384d92ecb65d2dc25bb0a15dea16ff44a47a9
Attiya <adarensburg@live.com>
2023-01-05 15:48:21 -0600

    image model modified

--- app/models/image.py
+++ app/models/image.py
@@ -4,8 +4,8 @@ class Image(db.Model):
     __tablename__ = 'images'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('posts.id')), nullable=False)
     image_url = db.Column(db.String(2200), nullable=False)
 
     post = db.relationship("Post", back_populates="images")
