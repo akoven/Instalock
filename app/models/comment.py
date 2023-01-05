6bf695bd8eee2f49de35e6feff7ac03b6729a770
Attiya <adarensburg@live.com>
2023-01-05 15:49:25 -0600

    comment model modified

--- app/models/comment.py
+++ app/models/comment.py
@@ -5,8 +5,8 @@ class Comment(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(2200), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('posts.id')), nullable=False)
     likes = db.Column(db.Integer, default=0)
 
     post = db.relationship("Post", back_populates="comments")
